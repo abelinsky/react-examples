a34848c3cc26c58be050ce51937f5051f5eb0035
Alexander Belinsky <squarrelhouse.ai@gmail.com>
2021-12-10 15:37:00 +0300

    Extend foodorder

--- foodorder/src/components/Layout/HeaderCartButton.js
+++ foodorder/src/components/Layout/HeaderCartButton.js
@@ -1,14 +1,40 @@
+import { useContext, useEffect, useState } from 'react';
+import CartContext from '../../store/cart-context';
 import CartIcon from '../Cart/CartIcon';
 import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = (props) => {
+  const [btnIsHighlited, setBtnIsHighlited] = useState(false);
+
+  const cartCtx = useContext(CartContext);
+  const numberOfCartItems = cartCtx.items.reduce((currNumber, item) => currNumber + item.amount, 0);
+
+  const { items } = cartCtx;
+
+  const btnClasses = `${classes.button} ${btnIsHighlited ? classes.bump : ''}`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnIsHighlited(true);
+
+    const timer = setTimeout(() => {
+      setBtnIsHighlited(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button className={classes.button}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
   );
 };
